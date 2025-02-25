983c209e316e0fb6f4e90618e6293795a4d80bd2
Fernando Belmonte <gualterbelmonte@gmail.com>
2025-02-25 00:04:34 -0400

    test: prueba numero 3 funcionando

---
+++ src/Romano.js
@@ -0,0 +1,11 @@
+function Romano(a) {
+    if(a===1){
+        return "i";
+    }
+    if(a===3){
+        return "iii";
+    }
+  }
+  
+  export default Romano;
+  
--- src/Romano.test.js
+++ src/Romano.test.js
@@ -1,7 +1,13 @@
 import Romano from "./Romano.js";
 
-describe("Romano", () => {
+describe("Romano convertidor", () => {
   it("deberia transformar 1 a i", () => {
     expect(Romano(1)).toEqual("i");
   });
 });
+
+describe("Romano convertidor", () => {
+    it("deberia transformar 3 a iii", () => {
+      expect(Romano(3)).toEqual("iii");
+    });
+  });
