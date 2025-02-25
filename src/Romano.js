b307f11a776ac5b76e102a75972efde1da4a52b8
Fernando Belmonte <gualterbelmonte@gmail.com>
2025-02-25 01:05:56 -0400

    test: prueba del 13 funcionando

--- src/Romano.js
+++ src/Romano.js
@@ -1,13 +1,47 @@
-function Romano(a) {
-    if(a===1){
+function numeros1_8(b){
+    if(b===1){
         return "i";
     }
-    if(a===3){
+    if(b===2){
+        return "ii";
+    }
+    if(b===3){
         return "iii";
     }
-    if(a===7){
+    if(b===4){
+        return "iv";
+    }
+    if(b===5){
+        return "v";
+    }
+    if(b===6){
+        return "vi";
+    }
+    if(b===7){
         return "vii";
     }
+    if(b===8){
+        return "viii";
+    }
+}
+
+
+function Romano(a) {
+    let b;
+    if(a<31 && a>0)
+    {
+        if(a<29 && a>18){
+
+        }
+        if(a<19 && a>8){
+            let unidades=a%10;
+            b = "X" + numeros1_8(unidades);
+        }
+        if(a<9 && a>0){
+            b = numeros1_8(a);
+        }
+    }
+    return b;
   }
   
   export default Romano;
--- src/Romano.test.js
+++ src/Romano.test.js
@@ -1,12 +1,12 @@
 import Romano from "./Romano.js";
 
-describe("Romano convertidor", () => {
+describe("Romano convertidor 1", () => {
   it("deberia transformar 1 a i", () => {
     expect(Romano(1)).toEqual("i");
   });
 });
 
-describe("Romano convertidor", () => {
+describe("Romano convertidor 3", () => {
     it("deberia transformar 3 a iii", () => {
       expect(Romano(3)).toEqual("iii");
     });
@@ -15,5 +15,11 @@ describe("Romano convertidor", () => {
 describe("Romano convertidor 7", () => {
     it("deberia transformar 7 a vii", () => {
       expect(Romano(7)).toEqual("vii");
+    });
+  });
+
+  describe("Romano convertidor 13", () => {
+    it("deberia transformar 7 a vii", () => {
+      expect(Romano(13)).toEqual("Xiii");
     });
   });
